f0077dfa4fea356c5d67fa6467c5859d17d485f2
Rayana Prata <50561896+rayanaprata@users.noreply.github.com>
2021-05-09 20:29:02 -0300

    exerccio

---
+++ exercicio4/src/exercicio4/LockBuffer.java
@@ -0,0 +1,60 @@
+package exercicio4;
+
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class LockBuffer implements Buffer {
+	private int buffer = -1; // shared by producer and consumer threads
+	private ReentrantLock mutex = new ReentrantLock();
+	private Condition cSet = mutex.newCondition();
+	private Condition cGet = mutex.newCondition();
+	
+	/*
+	private Condition c;
+	
+	void example() {
+		c.await();       // wait();
+		c.signal();      // notify();
+		c.signalAll();   // notifyAll()
+	}
+	*/
+
+	// place value into buffer
+	public void set(int value) {
+		try {
+			mutex.lock();
+			while (buffer != -1) {
+				cSet.await();
+			}
+			System.out.printf("Producer writes\t%2d", value);
+			buffer = value;
+			cGet.signalAll();
+		} catch (Exception e) {} 
+		finally {
+			mutex.unlock();
+		}
+
+	} // end method set
+
+	// return value from buffer
+	public int get() {
+
+		int val = -1;
+		
+		try {
+			mutex.lock();
+			while (buffer == -1) {
+				cGet.await();
+			}
+			val = buffer;
+			System.out.printf("Consumer reads\t%2d", buffer);
+			buffer = -1;
+			cSet.signalAll();
+		} catch (Exception e) {}
+		finally {
+			mutex.unlock();
+		}
+
+		return val;
+	} // end method get
+}
